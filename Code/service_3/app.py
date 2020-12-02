634c35ff98c9695e4d2d19750ade6397d40dc5d6
EfekanCalim1 <ecalim@qa.com>
2020-12-02 11:02:49 +0000

    added database feature and new structure

--- Code/service_3/app.py
+++ Code/service_3/app.py
@@ -1,12 +1,4 @@
-from flask import Flask, Response, request, jsonify
-import random
-
-app = Flask(__name__)
-
-@app.route('/weather', methods=['GET'])
-def weather():
-    weather_conditions: ["rain", "sun", "snow", "wind", "cloud"]
-    return Response(random.choices(weather_conditions), mimetype="text/plain")
+from application import app
 
 if __name__ =="__main__":
     app.run(debug=True, host='0.0.0.0', port=5002)
--- Code/service_4/app.py
+++ Code/service_4/app.py
@@ -1,8 +1,4 @@
-from flask import Flask, Response, request, jsonify
-import random
+from application import app
 
-app = Flask(__name__)
-
-@app.route('/suggestion', methods=['POST'])
-def suggestion():
-    city
+if __name__ =="__main__":
+    app.run(debug=True, host='0.0.0.0', port=5003)
