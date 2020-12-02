d872794f005b29e88de14fecf09490e5724c1a00
EfekanCalim1 <ecalim@qa.com>
2020-12-02 15:50:09 +0000

    small change to init file

--- Code/service_1/application/__init__.py
+++ Code/service_1/application/__init__.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+db = SQLAlchemy(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:rootpassword123@35.246.6.246/practical_db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
 
 from application import routes
