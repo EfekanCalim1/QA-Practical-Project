dd8075c3c6e6ed42df8246c125b3eef54d6e5237
EfekanCalim1 <ecalim@qa.com>
2020-12-04 11:14:27 +0000

    100% testing all services

--- Code/service_4/test/test_service_4.py
+++ Code/service_4/test/test_service_4.py
@@ -1,55 +1,77 @@
+from unittest.mock import patch
 from flask import url_for
 from flask_testing import TestCase
 
-from app import app 
+from app import app
 
 class TestBase(TestCase):
     def create_app(self):
         return app
 
-class TestWeather(TestBase):
-    def test_weather(self):
-        weather_conditions = [b"rain", b"sun", b"snow", b"wind", b"cloud"]
-        response = self.client.post(url_for('weather'))
-        self.assertIn(response.data, weather_conditions)
-
+class TestLondon(TestBase):
     def test_suggestion_sun(self):
-        response = self.client.post(
-            url_for('weather'),
-            data="sun",
-            follow_redirects=True
-        )
-        self.assertIn(b'It is a great time to visit', response.data)
-    
+        with patch("requests.get") as i:
+            i.return_value.text = 'London'
+            with patch("random.choices") as r:
+                r.return_value = 'sun'
+                response = self.client.post(
+                url_for('suggestion'),
+                data = 'sun London')
+                self.assertIn(b'No suggestion available', response.data)
+
+class TestTokyo(TestBase):
     def test_suggestion_rain(self):
-        response = self.client.post(
-            url_for('weather'),
-            data="rain",
-            follow_redirects=True
-        )
-        self.assertIn(b'Not a great time to visit', response.data)
+        with patch("requests.get") as i:
+            i.return_value.text = 'Tokyo'
+            with patch("random.choices") as r:
+                r.return_value = 'rain'
+                response = self.client.post(
+                url_for('suggestion'),
+                data = 'rain Tokyo')
+                self.assertIn(b'No suggestion available', response.data)
 
-    def test_suggestion_wind(self):
-        response = self.client.post(
-            url_for('weather'),
-            data="wind",
-            follow_redirects=True
-        )
-        self.assertIn(b'It could get a little brisk', response.data)
-
-    def test_suggestion_cloud(self):
-        response = self.client.post(
-            url_for('weather'),
-            data="cloud",
-            follow_redirects=True
-        )
-        self.assertIn(b"It's a bit dull, but at least it isn't raining", response.data)
-    
+class TestNewYork(TestBase):
     def test_suggestion_snow(self):
-        response = self.client.post(
-            url_for('weather'),
-            data="snow",
-            follow_redirects=True
-        )
-        self.assertIn(b"Wrap up warm", response.data)
+        with patch("requests.get") as i:
+            i.return_value.text = 'New York'
+            with patch("random.choices") as r:
+                r.return_value = 'sun'
+                response = self.client.post(
+                url_for('suggestion'),
+                data = 'snow New York')
+                self.assertIn(b'No suggestion available', response.data)
+
+class TestIstanbul(TestBase):
+    def test_suggestion_cloud(self):
+        with patch("requests.get") as i:
+            i.return_value.text = 'Istanbul'
+            with patch("random.choices") as r:
+                r.return_value = 'cloud'
+                response = self.client.post(
+                url_for('suggestion'),
+                data = 'cloud Istanbul')
+                self.assertIn(b'No suggestion available', response.data)
+
+class TestAmsterdam(TestBase):
+    def test_suggestion_wind(self):
+        with patch("requests.get") as i:
+            i.return_value.text = 'Amsterdam'
+            with patch("random.choices") as r:
+                r.return_value = 'wind'
+                response = self.client.post(
+                url_for('suggestion'),
+                data = 'wind Amsterdam')
+                self.assertIn(b'No suggestion available', response.data)
+
+class TestRome(TestBase):
+    def test_suggestion_sun(self):
+        with patch("requests.get") as i:
+            i.return_value.text = 'Rome'
+            with patch("random.choices") as r:
+                r.return_value = 'sun'
+                response = self.client.post(
+                url_for('suggestion'),
+                data = 'sun Rome')
+                self.assertIn(b'No suggestion available', response.data)
+    
       
